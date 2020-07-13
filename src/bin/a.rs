b5e03893858c154eacf454b8799df8de06c41c91
鎌田正聖 <masatoshi@masa-macbook-pro.local>
2020-07-13 19:45:14 +0900

    done a

--- src/bin/a.rs
+++ src/bin/a.rs
@@ -4,5 +4,33 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
+        n: usize,
+        a: [usize; n]
     }
+    let mut prev = false;
+    let mut next = false;
+    let mut count = 1;
+    let mut skip = true;
+    for i in 0..n - 1 {
+        if a[i] == a[i + 1] {
+            continue;
+        } else if a[i] < a[i + 1] {
+            next = true;
+        } else {
+            next = false;
+        }
+        if skip {
+            skip = false;
+            prev = next;
+            continue;
+        }
+        if prev == next {
+            continue;
+        } else {
+            count += 1;
+            prev = next;
+            skip = true;
+        }
+    }
+    println!("{}", count);
 }
